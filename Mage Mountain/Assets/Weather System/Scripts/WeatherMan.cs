216bb4ad1433de3f26a2426329760734551a0d57
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-04-10 00:42:10 -0400

    Work in progress

---
+++ Mage Mountain/Assets/Weather System/Scripts/WeatherMan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherMan : MonoBehaviour
+{
+    int mode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         *Sunny = 0
+         *Rainy = 1
+         *Snowy = 2
+         *Windy = 3
+         */
+        if (mode == 0)
+        {
+            //Sunny Mode
+        }else if (mode == 1)
+        {
+            //rainy Mode
+        }else if (mode == 2)
+        {
+            //Snowy Mode
+        
+        }else if (mode == 3)
+        {
+            //Windy mode
+        }
+        
+    }
+}
