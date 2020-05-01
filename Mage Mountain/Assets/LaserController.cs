a0719396153d4b714af7cc7339e4e8d2f5cc67b3
Ezekiel Oluwatobi Akande <64216829+ezekieltoby@users.noreply.github.com>
2020-05-01 16:13:50 -0400

    imported zence

---
+++ Mage Mountain/Assets/LaserController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    ParticleSystem beam;
+    ParticleSystem explo;
+    Rigidbody bod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bod = GetComponent<Rigidbody>();
+        beam = transform.Find("Beam").GetComponent<ParticleSystem>();
+        explo = transform.Find("Explo").GetComponent<ParticleSystem>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(bod);
+        Destroy(beam);
+        explo.Play();
+        Destroy(gameObject, 2);
+    }
+
+}
